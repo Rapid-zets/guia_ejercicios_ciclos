dc2b363d8e5a35a627f3e8be4fc9ed1da18df4ea
Rapid <134418161+Rapid-zets@users.noreply.github.com>
2024-04-18 08:27:48 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rapidzets.ismael_herrera_guia_20;
+
+/**
+ *
+ * @author ismae
+ */
+public class Main {
+    public static void main(String[] args) {
+        Ismael_herrera_guia_20.main(args);
+    }
+    
+}
